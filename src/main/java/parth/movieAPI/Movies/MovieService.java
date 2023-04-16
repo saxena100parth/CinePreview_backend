a3c472bfdac1c235410f00f66574535fec432300
saxena100parth <87128985+saxena100parth@users.noreply.github.com>
2023-04-16 19:07:27 +0530

    initial backend commit

---
+++ src/main/java/parth/movieAPI/Movies/MovieService.java
@@ -0,0 +1,22 @@
+package parth.movieAPI.Movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+
+    }
+    public Optional<Movie> singleMovie(String ImdbId){
+        return movieRepository. findMovieByImdbId(ImdbId);
+    }
+
+}
