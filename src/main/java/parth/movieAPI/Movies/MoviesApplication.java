022468d2ac23f50d321e4a0d82a38140dd610599
saxena100parth <87128985+saxena100parth@users.noreply.github.com>
2023-04-16 22:09:00 +0530

    udating cors property

--- src/main/java/parth/movieAPI/Movies/MoviesApplication.java
+++ src/main/java/parth/movieAPI/Movies/MoviesApplication.java
@@ -2,7 +2,10 @@ package parth.movieAPI.Movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 
@@ -10,6 +13,15 @@ public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
